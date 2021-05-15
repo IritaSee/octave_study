5ce0167ed3ebdb9745e083a6f079db06ce288ae3
IritaSee <iga.narendra@gmail.com>
2021-05-15 13:45:35 +0800

    add frontend frame

--- glcm_features_extraction.m
+++ glcm_features_extraction.m
@@ -3,6 +3,30 @@ function glcm_features_extraction()
   ax1=axes('units','normalize','position',[0.3 0.6 0.3 0.3]);
   push1=uicontrol('units','normalize','style','pushbutton','string','buka file','position',[0.1 0.8 0.15 0.1],'callback',{@bukafile ax1});
   push2=uicontrol('units','normalize','style','pushbutton','string','convert','position',[0.7 0.8 0.15 0.1],'callback', {@convert,ax1});
+  push3=uicontrol('units','normalize','style','pushbutton','string','process','position',[0.3 0.5 0.15 0.05]);
+  
+  txt1=uicontrol('units','normalize','style','text','string','distance :','position',[0.7 0.7 0.1 0.05]);
+  txt2=uicontrol('units','normalize','style','text','string','angle :','position',[0.7 0.65 0.1 0.05]);
+  txt3=uicontrol('units','normalize','style','text','string','levels :','position',[0.7 0.6 0.1 0.05]);
+  
+  edit1=uicontrol('units','normalize','style','edit','position',[0.8 0.7 0.05 0.05]);
+  edit2=uicontrol('units','normalize','style','edit','position',[0.8 0.65 0.05 0.05]);
+  edit3=uicontrol('units','normalize','style','edit','position',[0.8 0.6 0.05 0.05]);
+  
+  contrast_text=uicontrol('units','normalize','style','text','string','contrast :','position',[0.1 0.4 0.1 0.05]);
+  contrast_val=uicontrol('units','normalize','style','edit','position',[0.1 0.35 0.1 0.05],'enable','off');
+  
+  %diss_text=uicontrol('units','normalize','style','text','string','dissimilarity :','position',[0.1 0.4 0.1 0.05]);
+  %diss_val=uicontrol('units','normalize','style','edit','position',[0.1 0.35 0.1 0.05],'enable','off');
+  
+  %homogenity_text=uicontrol('units','normalize','style','text','string','homogenity :','position',[0.1 0.4 0.1 0.05]);
+  %homogenity_val=uicontrol('units','normalize','style','edit','position',[0.1 0.35 0.1 0.05],'enable','off');
+  
+  %asm_text=uicontrol('units','normalize','style','text','string','asm :','position',[0.1 0.4 0.1 0.05]);
+  %asm_val=uicontrol('units','normalize','style','edit','position',[0.1 0.35 0.1 0.05],'enable','off');
+  
+  %energy_text=uicontrol('units','normalize','style','text','string','energy :','position',[0.1 0.4 0.1 0.05]);
+  %energy_val=uicontrol('units','normalize','style','edit','position',[0.1 0.35 0.1 0.05],'enable','off');
 endfunction
 
 
@@ -25,24 +49,24 @@ endfunction
   
 
 
-function Y=glcm(X, dx, dy, grayScale)  
+%function Y=glcm(X, dx, dy, grayScale)  
   % (dx, dy):
 %   ( 1, 0)   0°
 %   ( 0, 1)  45°
 %   ( 1, 1)  90°
 %   (-1, 1) 135°
 
-[m, n] = size(X);
-maxGrayScale = max(X) + 1;
-X = floor(X.*grayScale./maxGrayScale);
-Y = zeros(grayScale,grayScale);
-for i = 1:m-dy
-    for j = 1:n-dx;
-        r = X(i,j)+1;
-        c = X(i+dy, j+dx)+1;
-        Y(r,c) = Y(r,c)+1;
-    end
-end
-Y = Y./(m*n);
+%[m, n] = size(X);
+%maxGrayScale = max(X) + 1;
+%X = floor(X.*grayScale./maxGrayScale);
+%Y = zeros(grayScale,grayScale);
+%for i = 1:m-dy
+%    for j = 1:n-dx;
+%        r = X(i,j)+1;
+%        c = X(i+dy, j+dx)+1;
+%        Y(r,c) = Y(r,c)+1;
+%    end
+%end
+%Y = Y./(m*n);
 
-end
+%end
